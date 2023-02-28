8496925b5eee1af92bb2266292cf3e600897026b
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-02-27 23:35:04 -0300

    feat: :sparkles: cria página de um post expecifico buscado na API pelo seu identificador slug

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import { marked } from "marked";
+import { RenderMarkdown } from "thon-ui";
+
+interface PostProps {
+    params: {
+        slug: string
+    }
+}
+
+const baseURL = 'https://www.tabnews.com.br/api/v1';
+const postsEndPoint = '/contents/rafaelsreis'
+
+async function getLastPost(slug: string) {
+    const response = await fetch(`${baseURL}${postsEndPoint}/${slug}`);
+    const posts = await response.json()
+    return posts
+}
+
+export default async function Post({ params }: PostProps) {
+
+    const post = await getLastPost(params.slug)
+    console.log(post)
+    const formatHTML = marked.parse(post.body)
+
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <div>
+                <RenderMarkdown value={post.body} />
+            </div>
+        </>
+    )
+}
