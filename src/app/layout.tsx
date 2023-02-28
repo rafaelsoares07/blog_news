a3c20abd4e33194858c253a7cd11f552f38c126b
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-02-27 23:52:27 -0300

    feat: :bug: mudando propriedade crosOrigin de importação de fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
